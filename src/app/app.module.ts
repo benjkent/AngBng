1493fefe829d8ed19dab858aa1a3a33769c8e4aa
Benjamin Kent <benjaminfranklinkent@gmail.com>
2022-11-16 11:52:41 -0800

    add shipping prices using HttpClientModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ProductDetailsComponent } from './product-details/product-details.component';
 import { ProductListComponent } from './product-list/product-list.component';
 import { TopBarComponent } from './top-bar/top-bar.component';
 import { CartComponent } from './cart/cart.component';
+import { ShippingComponent } from './shipping/shipping.component';
 
 @NgModule({
   declarations: [
@@ -14,11 +16,13 @@ import { CartComponent } from './cart/cart.component';
     ProductDetailsComponent,
     ProductListComponent,
     TopBarComponent,
-    CartComponent
+    CartComponent,
+    ShippingComponent
   ],
   imports: [
     RouterModule,
     BrowserModule,
+    HttpClientModule,
     AppRoutingModule
   ],
   providers: [],
